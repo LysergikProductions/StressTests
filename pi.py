83f7cc7d8de878a009257373c7f4472b177e4c2f
LysergikProductions <61183022+LysergikProductions@users.noreply.github.com>
2020-05-30 14:36:46 -0400

    Fixed some bugs and cleaned code a bit

--- pi.py
+++ pi.py
@@ -5,23 +5,44 @@ pi.py
 
 # Build_002
 
-                   -- Purpose --
- Stress test CPU for its sigle-threaded performance
+                                          -- Purpose --
+ Simplify the process of installing and make it as convenient as possible to install builds on Android devices, via Android Debug Bridge
 
 """
 
 import os; import sys
 import decimal; import time
 startTime = time.time()
-txtPi = ""
+txtPi = ""; final_k = 1; its = 1
+decimalArgs = decimalArgs = decimal.Decimal(0)
+
+def INIT():
+	global decimalArgs
+	os.system('clear && printf "Initializing..\n"')
+	try:
+		args = int(sys.argv[1]); print(args)
+		decimalArgs = decimal.Decimal(args)
+	except:
+		print("""I can't do that! I need a single number with no extra symbols!\n""")
+	finally:
+		if decimalArgs < 1:
+			decimalArgs = 1
+		elif decimalArgs > 10000000:
+			decimalArgs = 10000000
+		elif not decimalArgs:
+			print('you need to enter a number!')
+
+INIT()
 
 def getUptime():
 	return time.time() - startTime
 
 def compute_pi(n):
 	decimal.getcontext().prec = n + 1
+	global final_k
 
-	n += 1; x = 0
+	its = n; n += 11
+	x = 0
 	C = 426880 * decimal.Decimal(10005).sqrt()
 
 	K = 6.0; M = 1.0; X = 1
@@ -40,8 +61,8 @@ def compute_pi(n):
 		print('{0}\n'.format(pi))
 
 		if lastPi != pi:
-			print('Accuracy improved from iteration {0}'.format(k + 1))
-			final_k = k + 1
+			print('Accuracy improved from iteration {0}'.format(k))
+			final_k = k
 		else:
 			x += 1
 			print("Pi not changed {0} time/s".format(x))
@@ -49,29 +70,16 @@ def compute_pi(n):
 			if x >= int(10):
 				os.system('history -c && clear')
 				print(pi)
-				print("\n{0} digits of precision achieved in {1} iterations!".format(n - 1, final_k))
+				print("\n{0} digits of precision achieved in {1} iterations!".format(its, final_k))
 
 				global txtPi; txtPi = str(pi)
 				return ""
-
-os.system('clear && printf "Initializing..\n"')
-args = int(sys.argv[1]); print(args)
-try: decimalArgs = decimal.Decimal(args)
-except:
-	print("""I can't do that! I need a single number with no extra symbols!\n""")
-finally:
-	if decimalArgs < 1:
-		decimalArgs = 1
-	elif decimalArgs > 10000000:
-		decimalArgs = 10000000
-	elif not decimalArgs:
-		print('you need to enter a number!')
 
 def MAIN():
 	result = compute_pi(int(decimalArgs)); print(result)
 	f = open('pi.txt', 'w+'); f.write(txtPi)
 
 	uptime = str(getUptime())
-	print("This took {0} seconds".format(uptime))
+	print("This took {0} seconds\n".format(uptime))
 
 MAIN()
